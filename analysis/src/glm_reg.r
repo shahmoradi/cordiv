36f73e86a36371fe407b090b1111e3ec450e67f8
Amir Shahmoradi <a.shahmoradi@gmail.com>
2014-10-24 17:17:05 -0500

    regularized_regression tables and figures added.

--- analysis/src/glm_reg.r
+++ analysis/src/glm_reg.r
@@ -1,7 +1,7 @@
 # This R code performs regularized regression on pdb properties data.
 # Amir Shahmoradi, Sunday 4:47 AM, Oct 19 2014, Wilke Lab, ICMB, UT Austin
 
-install.packages("glmnet", repos = "http://cran.us.r-project.org")
+# install.packages("glmnet", repos = "http://cran.us.r-project.org")
 library(glmnet)
 
 setwd('C:/Users/Amir/Documents/GitHub/cordiv/analysis/src')
@@ -12,10 +12,56 @@ seqent_scors = grep('^r.+seqent',colnames(all_pdb_prop_subset),value = TRUE)  # 
 r4sJC_scors = grep('^r.+r4sJC',colnames(all_pdb_prop_subset),value = TRUE)  # Select column names that correspond to seqent-variable correlations
 not_in_pcr = c(seqent_scors,r4sJC_scors)
 
-fit = glmnet(x = as.matrix(all_pdb_prop_subset[,-which(names(all_pdb_prop_subset) %in% not_in_pcr)]), y = as.vector(all_pdb_prop_subset$r.seqent.wcnSC))
+x = as.matrix(all_pdb_prop_subset[,-which(names(all_pdb_prop_subset) %in% not_in_pcr)])   # predictor variables
+
+for (correlation in not_in_pcr)
+{
+  cat (paste(' Now fitting for correlation:', correlation, '\n'))
+  #fit = glmnet(x = x,
+  #             y = as.vector(all_pdb_prop_subset[[correlation]]),
+  #             alpha = 0.5   # elastic-net mixing parameter, with a range from 0 (ridge regression) to 1 (lasso regression).
+  #             )
+
+  cvfit = cv.glmnet(x = x,
+                    y = as.vector(all_pdb_prop_subset[[correlation]]),
+                    alpha = 0.5   # elastic-net mixing parameter, with a range from 0 (ridge regression) to 1 (lasso regression).
+                    )
+  
+  lambda.min.coef = data.frame(as.matrix(coef(cvfit, s = 'lambda.min')))
+  lambda.min.coef = data.frame(variable = row.names(lambda.min.coef),
+                               coef     = lambda.min.coef$X1,
+                               abs.coef = abs(lambda.min.coef$X1)
+                               )
+  lambda.min.coef = lambda.min.coef[with(lambda.min.coef, order(-abs.coef)),]
+
+  lambda.1se.coef = data.frame(as.matrix(coef(cvfit, s = 'lambda.1se')))
+  lambda.1se.coef = data.frame(variable = row.names(lambda.1se.coef),
+                               coef     = lambda.1se.coef$X1,
+                               abs.coef = abs(lambda.1se.coef$X1)
+                               )
+  lambda.1se.coef = lambda.1se.coef[with(lambda.1se.coef, order(-abs.coef)),]
+  
+  filename = paste0('../tables/regularized_regression/',correlation,'_lmin.csv')
+  write.csv(lambda.min.coef, filename, row.names = F)
+  
+  filename = paste0('../tables/regularized_regression/',correlation,'_l1se.csv')
+  write.csv(lambda.1se.coef, filename, row.names = F)
+  
+  # Now generate the error-lambda plots:
+  
+  filename = paste0('../figures/regularized_regression/',correlation,'.pdf')
+  pdf( filename, width=6, height=5, useDingbats=FALSE )
+  plot(cvfit)
+  dev.off()  
+  #cvfit$lambda.min
+}
+
+
+
 predictor_names = colnames(all_pdb_prop_subset[,-which(names(all_pdb_prop_subset) %in% not_in_pcr)])
-predictor_names[145]
+predictor_names[103]
 plot(fit, label = TRUE)
+plot(fit, xvar = 'lambda', label = TRUE)
 print(fit)
 coef(fit,s=0.1)
 
