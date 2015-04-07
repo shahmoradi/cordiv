00e1e72721b98980f981a9cb5aeaa18efef816fe
Amir Shahmoradi <a.shahmoradi@gmail.com>
2015-04-07 00:32:06 -0500

    New figures and tables for WCN definition

--- wcn_best_definition/analysis/src/get_quantiles.r
+++ wcn_best_definition/analysis/src/get_quantiles.r
@@ -40,6 +40,56 @@ dflist = list( wcneSC_bfSC   = exp_wcneSC_bfSC
 counter = 0
 for (dataframe in dflist){
   counter = counter + 1
+  
+  ###############################################################################
+  ###############################################################################  
+  # First write out the quantiles for the raw values of Spearman correlation strengths:
+  quantiles = data.frame()
+  for (i in names(dataframe[,-1])){
+     row = data.frame(parameter     = dataframe[[i]][1],
+                      mean_sp       = mean(dataframe[[i]][-1]),
+                      median_sp     = quantile(as.vector(dataframe[[i]][-1]), probs = 0.50),
+                      quantile05_sp = quantile(as.vector(dataframe[[i]][-1]), probs = 0.05),
+                      quantile25_sp = quantile(as.vector(dataframe[[i]][-1]), probs = 0.25),
+                      quantile75_sp = quantile(as.vector(dataframe[[i]][-1]), probs = 0.75),
+                      quantile95_sp = quantile(as.vector(dataframe[[i]][-1]), probs = 0.95),
+                      stdev_sp      = sd(as.vector(dataframe[[i]][-1]))
+                      )
+     quantiles = rbind(quantiles,row)
+  }
+  rownames(quantiles) = NULL
+  write.csv( quantiles, file = paste0('../tables/get_quantiles/sp_raw/',names(dflist)[[counter]],'_quantiles.csv'), row.names=F)
+  
+  # Now generate the plot:
+  model = substr(names(dflist)[[counter]],start=4,stop=4)
+  atom  = substr(names(dflist)[[counter]],start=5,stop=6)
+  var   = substr(names(dflist)[[counter]],start=8,stop=9)
+  if (model == "e") {main_lab = "Exponential WCN"; xlab = 'Exponential Mean [Angstroms]'}
+  if (model == "g") {main_lab = "Gaussian WCN"; xlab = 'Scale parameter [Angstroms]'}
+  if (model == "h") {main_lab = "Heaviside WCN"; xlab = 'Cutoff Distance [Angstroms]'}
+  if (model == "p") {main_lab = "Power-law WCN"; xlab = 'Power-law Exponent'; quantiles = quantiles[!quantiles$parameter==0.0,] }
+  if (var == "bf") ylab = expression( paste( "Spearman ", rho ," : WCN - B factor" ) )
+  if (var == "dd") ylab = expression( paste( "Spearman ", rho ," : WCN - ddG Rate" ) )
+  if (var == "r4") ylab = expression( paste( "Spearman ", rho ," : WCN - r4sJC" ) )
+  if (var == "se") ylab = expression( paste( "Spearman ", rho ," : WCN - Seq. Entropy" ) )
+  
+  #Plot Average raw Spearman correlation vs. value of free parameter
+  pdf( paste0("../figures/get_quantiles/sp_raw/spcor_",names(dflist)[[counter]],".pdf"), width=4.5, height=4, useDingbats=FALSE )
+  #par( mai=c(0.65, 0.65, 0.1, 0.05), mgp=c(2, 0.5, 0), tck=-0.03 )
+  plot(quantiles$parameter,quantiles$mean_sp, type = 'l', ylim=c(-0.9,0.9), xlab=xlab, ylab=ylab, main=main_lab)
+  polygon(c(quantiles$parameter,rev(quantiles$parameter)),c(quantiles$quantile25_sp,rev(quantiles$quantile75_sp)),col = "green", border = FALSE)
+  lspl = smooth.spline(quantiles$parameter,quantiles$quantile25_sp)
+  uspl = smooth.spline(quantiles$parameter,quantiles$quantile75_sp)
+  lines(quantiles$parameter,quantiles$mean_sp,lwd=2)
+  lines(quantiles$parameter,quantiles$median_sp,lwd=2,lty=2)
+  #lines(predict(lspl,quantiles$parameter),lty=2,lwd=2,col='red')
+  #lines(predict(uspl,quantiles$parameter),lty=2,lwd=2,col='red')
+  graphics.off()
+
+
+  ###############################################################################
+  ###############################################################################
+  # Now write out the quantiles for the absolute values of Spearman correlation strengths:
   quantiles = data.frame()
   for (i in names(dataframe[,-1])){
      row = data.frame(parameter     = dataframe[[i]][1],
@@ -54,14 +104,14 @@ for (dataframe in dflist){
      quantiles = rbind(quantiles,row)
   }
   rownames(quantiles) = NULL
-  write.csv( quantiles, file = paste0('../tables/',names(dflist)[[counter]],'_quantiles.csv'), row.names=F)
-  
+  write.csv( quantiles, file = paste0('../tables/get_quantiles/sp_abs/',names(dflist)[[counter]],'_quantiles.csv'), row.names=F)
+
   # Now generate the plot:
   model = substr(names(dflist)[[counter]],start=4,stop=4)
   atom  = substr(names(dflist)[[counter]],start=5,stop=6)
   var   = substr(names(dflist)[[counter]],start=8,stop=9)
   if (model == "e") {main_lab = "Exponential WCN"; xlab = 'Exponential Mean [Angstroms]'}
-  if (model == "g") {main_lab = "Gaussian WCN"; xlab = 'Standard Deviation [Angstroms]'}
+  if (model == "g") {main_lab = "Gaussian WCN"; xlab = 'Scale parameter [Angstroms]'}
   if (model == "h") {main_lab = "Heaviside WCN"; xlab = 'Cutoff Distance [Angstroms]'}
   if (model == "p") {main_lab = "Power-law WCN"; xlab = 'Power-law Exponent'; quantiles = quantiles[!quantiles$parameter==0.0,] }
   if (var == "bf") ylab = expression( paste( "Absolute Spearman ", rho ," : WCN - B factor" ) )
@@ -69,10 +119,10 @@ for (dataframe in dflist){
   if (var == "r4") ylab = expression( paste( "Absolute Spearman ", rho ," : WCN - r4sJC" ) )
   if (var == "se") ylab = expression( paste( "Absolute Spearman ", rho ," : WCN - Seq. Entropy" ) )
   
-  #Plot Average Spearman correlation vs. value of free parameter
-  pdf( paste0("../figures/spcor_",names(dflist)[[counter]],".pdf"), width=4.5, height=4, useDingbats=FALSE )
+  #Plot Average Absolute Spearman correlation vs. value of free parameter
+  pdf( paste0("../figures/get_quantiles/sp_abs/spcor_",names(dflist)[[counter]],".pdf"), width=4.5, height=4, useDingbats=FALSE )
   #par( mai=c(0.65, 0.65, 0.1, 0.05), mgp=c(2, 0.5, 0), tck=-0.03 )
-  plot(quantiles$parameter,quantiles$mean_sp, type = 'l', ylim=c(0.,0.9), xlab=xlab, ylab=ylab, main=main_lab)
+  plot(quantiles$parameter,quantiles$mean_sp, type = 'l', ylim=c(0.0,0.9), xlab=xlab, ylab=ylab, main=main_lab)
   polygon(c(quantiles$parameter,rev(quantiles$parameter)),c(quantiles$quantile25_sp,rev(quantiles$quantile75_sp)),col = "green", border = FALSE)
   lspl = smooth.spline(quantiles$parameter,quantiles$quantile25_sp)
   uspl = smooth.spline(quantiles$parameter,quantiles$quantile75_sp)
@@ -82,12 +132,15 @@ for (dataframe in dflist){
   lines(predict(uspl,quantiles$parameter),lty=2,lwd=2,col='red')
   graphics.off()
 
+
+  ###############################################################################
+  ###############################################################################
   #Plot Standard deviation of Spearman correlations vs. value of free parameters:
   if (var == "bf") ylab = expression( paste( "St. Dev. Spearman ", rho ," : WCN - B factor" ) )
   if (var == "dd") ylab = expression( paste( "St. Dev. Spearman ", rho ," : WCN - ddG Rate" ) )
   if (var == "r4") ylab = expression( paste( "St. Dev. Spearman ", rho ," : WCN - r4sJC" ) )
   if (var == "se") ylab = expression( paste( "St. Dev. Spearman ", rho ," : WCN - Seq. Entropy" ) )
-  pdf( paste0("../figures/stdev_",names(dflist)[[counter]],".pdf"), width=4.5, height=4, useDingbats=FALSE )
+  pdf( paste0("../figures/get_quantiles/stdev_",names(dflist)[[counter]],".pdf"), width=4.5, height=4, useDingbats=FALSE )
   #par( mai=c(0.65, 0.65, 0.1, 0.05), mgp=c(2, 0.5, 0), tck=-0.03 )
   plot(quantiles$parameter,quantiles$stdev_sp, type = 'l', lwd=2, xlab=xlab, ylab=ylab, main=main_lab)
   #lines(predict(lspl,quantiles$parameter),lty=2,lwd=2,col='red')
