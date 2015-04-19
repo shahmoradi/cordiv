ce4ad6bed81346a601bec7c0e0963f9bc3b67a64
Amir Shahmoradi <a.shahmoradi@gmail.com>
2015-04-18 19:12:56 -0500

    PDB gyration radii calculated and added normalized distances from COM added.

--- properties/src/get_res_prop_dist_from_COM.f90
+++ properties/src/get_res_prop_dist_from_COM.f90
@@ -1,10 +1,12 @@
 ! This Fortran program attempts to find the distance of individual residues from the geometrical center of the protein.
-! This code used the output data from get_pdb_prop_CO.f90 and get_res_crd_bf.py
+! This code now also outputs the radius of gyration of proteins ina separater file.
+! This code uses the output data from get_pdb_prop_CO.f90 and get_res_crd_bf.py
 
 ! INPUT:
 !       -- Path to the input file pdb_prop_CO.out containing pdb names and number of residues
 !       -- Path to the input representative atomic coordinates and Bfactors (../properties/res_crd_bf)
 !       -- Path and name of the output file containing distance from COM of each residue for all pdb structures.
+!       -- Path and name of the output file containing the radii of gyrations of PDBs.
 
 ! Amir Shahmoradi, Saturday 1:27 PM, April 18 2015, iCMB, UT Austin
 
@@ -18,7 +20,8 @@ implicit none
   integer         , dimension(:)  , allocatable :: res_num
   real*8          , dimension(:,:), allocatable :: crd
   real*8          , dimension(:)  , allocatable :: bfactor,bfactor_min,bfactor_max,distance
-  real*8          , dimension(3)                :: COM  ! geometrical center of protein
+  real*8          , dimension(3)                :: COM                     ! geometrical center of protein
+  real*8                                        :: radius_of_gyration      ! geometrical center of protein
   character(len=6)                              :: pdb,pdb_name            ! The name of the pdb structure and the SINGLE chain contained in it.
   
   
@@ -26,11 +29,12 @@ implicit none
   character(len=300) :: co_in                                              ! input pdb file. ATTN: Any non ATOM records will be ignored!
   character(len=300) :: crd_in                                             ! input pdb file. ATTN: Any non ATOM records will be ignored!
   character(len=300) :: output                                             ! output file containing distances of residues from COM of proteins
+  character(len=300) :: rogout                                             ! output file containing radii of gyrations
   
 ! input files variables:
   integer            :: iostat, counter
   integer            :: co_in_unit=10,crd_in_unit=11                       ! input file units
-  integer            :: output_unit=21                                     ! output file units
+  integer            :: output_unit=21,rogout_unit=22                      ! output file units
   integer            :: ios
 
 ! other variables:
@@ -38,7 +42,7 @@ implicit none
   real*8 :: tstart,tend
 
 ! First get the command line arguments
-  if (command_argument_count()/=3) then
+  if (command_argument_count()/=4) then
     write(*,*)
     write(*,*) "Incorrect number of input arguments on the command line."
     write(*,*) "Correct use:"
@@ -49,6 +53,7 @@ implicit none
   call get_command_argument(1,co_in)
   call get_command_argument(2,crd_in)
   call get_command_argument(3,output)
+  call get_command_argument(4,rogout)
   
 ! OPEN INPUT FILES:
   open(unit=co_in_unit,file=trim(adjustl(co_in)),status='old')
@@ -57,7 +62,10 @@ implicit none
   read(crd_in_unit,*)       ! read file header
 
   open(unit=output_unit,file=trim(adjustl(output)),status='replace')
-  write(output_unit,'(7A25)') 'pdb','resnam','resnum','distance_from_COM','bf','bfmin','bfmax'
+  write(output_unit,'(8A25)') 'pdb','resnam','resnum','distance_from_COM','distance_normalized','bf','bfmin','bfmax'
+  
+  open(unit=rogout_unit,file=trim(adjustl(rogout)),status='replace')
+  write(rogout_unit,'(3A25)') 'pdb','nres','radius_gyration'
   
 call cpu_time(tstart)
 do ii = 1,npdb
@@ -79,11 +87,19 @@ do ii = 1,npdb
   do i = 1,3
     com(i) = sum(crd(i,1:nres)) / dble(nres)
   end do
+  radius_of_gyration = 0.d0
   do i = 1,nres
     distance(i) = sqrt( (crd(1,i)-com(1))**2 + (crd(2,i)-com(2))**2 + (crd(3,i)-com(3))**2 )
-    ! NOW WRITE OUT THE FIRST FOUR LINES OF THE OUTPUT WCN FILE
-    write(output_unit,'(2A25,1I25,4E25.5)') pdb, res_name(i), res_num(i), distance(i), bfactor(i), bfactor_min(i), bfactor_max(i)
+    radius_of_gyration = radius_of_gyration + distance(i)*distance(i)
   end do
+   radius_of_gyration = sqrt( radius_of_gyration / dble(nres) )
+
+  do i = 1,nres
+    ! NOW WRITE OUT THE FIRST FOUR LINES OF THE OUTPUT WCN FILE
+    write(output_unit,'(2A25,1I25,5E25.5)') pdb, res_name(i), res_num(i), distance(i), distance(i)/radius_of_gyration, bfactor(i), bfactor_min(i), bfactor_max(i)
+  end do
+  
+  write(rogout_unit,'(1A25,1I25,1E25.5)') pdb, nres, radius_of_gyration
   
   deallocate(crd,bfactor,bfactor_min,bfactor_max,distance,res_name,res_num)
     
@@ -95,5 +111,6 @@ write(*,*) new_line('a'), 'Overall it took ', tend-tstart, ' seconds for all 213
 close(co_in_unit)
 close(crd_in_unit)
 close(output_unit)
+close(rogout_unit)
 
 end program get_res_prop_dist_from_COM
